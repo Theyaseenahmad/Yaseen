11c4b9a3518caeb33a7053a7c43b8e5af511fd58
Yaseen Ahmad <yaseenahmad02.777@gmail.com>
2025-03-11 20:28:54 +0530

    order change

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,11 +12,12 @@ const Projects = ({projectRef}) => {
     <div ref={projectRef} className=" mt-32 min-h-screen w-full flex justify-center items-center">
         <div className='md:w-[70%] w-full  min-h-screen gap-14  flex flex-col justify-start items-start md:p-4 '>
           <h2 className='font-bold text-3xl p-4 md:p-0 text-slate-600 uppercase'>Projects</h2>
-          <Ecom></Ecom>
-          <ChessCard></ChessCard>
           <CrowdFunds></CrowdFunds>
           <Genform></Genform>
           <TrueFeedback></TrueFeedback>
+          <Ecom></Ecom>
+          <ChessCard></ChessCard>
+         
           <Saas></Saas>
           {/* <UberExt></UberExt> */}
           
