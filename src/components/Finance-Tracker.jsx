70d394e15eb60cc9b67a2eeaeb98aa7317779d6e
Yaseen Ahmad <yaseenahmad02.777@gmail.com>
2025-03-22 15:18:32 +0530

    add projects

---
+++ src/components/Finance-Tracker.jsx
@@ -0,0 +1,77 @@
+import gsap from "gsap"
+import { useRef } from "react";
+import { LuArrowBigRight } from "react-icons/lu";
+
+
+
+
+const FinanceTracker = () => {
+
+  const handleMouseEnter = ()=>{
+    // make cursor pointer
+
+    
+    gsap.to(circle.current,{
+      zIndex: 20,
+      opacity:1
+    })
+
+  }
+  const handleMouseLeave = ()=>{
+    gsap.to(circle.current,{
+      zIndex: -10,
+      opacity:0
+    })
+  }
+
+  const card = useRef(null)
+  const circle = useRef(null)
+  const handleMouseMove = (e)=>{
+    if (card.current && circle.current) {
+      const cardRect = card.current.getBoundingClientRect(); // Get card's position
+      const offsetX = e.clientX - cardRect.left; // Mouse x position relative to card
+      const offsetY = e.clientY - cardRect.top;  // Mouse y position relative to card
+
+      gsap.to(circle.current, {
+        x: offsetX - circle.current.offsetWidth / 2, // Center the circle on the mouse
+        y: offsetY - circle.current.offsetHeight / 2, // Center the circle on the mouse
+        zIndex: 20,
+      });
+    }
+  }
+
+  
+  return (
+   <div className="w-full flex  items-center justify-center">
+
+{/* main card */}
+     <div ref={card} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} onMouseMove={handleMouseMove} id="card"  className="flex relative gap-1 md:w-[75%] w-[90%] bg-black  h-60 justify-center  rounded-lg">
+      
+            <a href="https://finance-tracker-phi-sage.vercel.app/" className="absolute top-1 right-3  md:hidden text-base font-bold flex justify-center items-center  p-2"><h1 className="bg-gradient-to-t from-[#cfc09f] via-[#95771e] to-[#ffae0b] text-transparent bg-clip-text">Visit</h1> <LuArrowBigRight className="text-[#ffae0b]" /></a>
+
+      
+
+<a href="https://finance-tracker-phi-sage.vercel.app/" id="circle" ref={circle} className="absolute hover:cursor-pointer md:flex md:flex-col hidden md:visible justify-start items-start top-0 left-0 size-32  rounded-full bg-slate-950 p-6 text-md font-bold -z-40 border-[2px] border-[#ffae0b]"><h2 className="bg-gradient-to-t from-[#cfc09f] via-[#95771e] to-[#ffae0b] text-transparent bg-clip-text">View </h2><h2 className="bg-gradient-to-t from-[#cfc09f] via-[#95771e] to-[#ffae0b] text-transparent bg-clip-text">Website</h2></a>
+
+  <div  className="w-[33%] h-full rounded-tl-lg rounded-bl-lg overflow-hidden">
+    <img src="\assets\Fin.png" className="w-full h-full object-cover" alt="" />
+  </div>
+  
+  <div  className="w-[67%] p-4 h-full bg-[#222222] rounded-tr-lg rounded-br-lg flex flex-col justify-start items-start gap-2">
+
+    <div className="h-2 rounded-xl border-[1px] bg-[#111111] border-gray-700 p-2 flex items-center justify-center"><h5 className="text-xs font-sans tracking-tighter">React & Recharts</h5></div>
+
+    <div><h2 className="md:text-2xl font-bold text-white">
+      Finance Tracker : Personal Finance Tracker</h2>
+      </div>
+
+      <div><p className="text-xs font-semibold">A modern financial management application with real-time analytics and budget tracking, built using Next.js App Router and modern web technologies.</p></div>
+
+  </div>
+  
+</div>
+   </div>
+  )
+}
+
+export default FinanceTracker
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,23 +2,27 @@ import PropTypes from 'prop-types'
 import ChessCard from './ChessCard'
 import CrowdFunds from './CrowdFunds'
 import TrueFeedback from './TrueFeedback'
-import Saas from './Saas'
 import Ecom from './Ecom'
 // import UberExt from './UberExt'
 import Genform from './GenForm'
+import FinanceTracker from './Finance-Tracker'
+import Coupon from './Coupon'
+import Email from './Email'
 
 const Projects = ({projectRef}) => {
   return (
     <div ref={projectRef} className=" mt-32 min-h-screen w-full flex justify-center items-center">
         <div className='md:w-[70%] w-full  min-h-screen gap-14  flex flex-col justify-start items-start md:p-4 '>
           <h2 className='font-bold text-3xl p-4 md:p-0 text-slate-600 uppercase'>Projects</h2>
+          <FinanceTracker></FinanceTracker>
+          <Coupon></Coupon>
+          <Email></Email>
           <CrowdFunds></CrowdFunds>
           <Genform></Genform>
           <TrueFeedback></TrueFeedback>
           <Ecom></Ecom>
           <ChessCard></ChessCard>
          
-          <Saas></Saas>
           {/* <UberExt></UberExt> */}
           
           
